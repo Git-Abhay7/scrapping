499dd19fe8d08b45c2f0049e3f27fc73d8bfc1e4
Git-Abhay7 <abhayrajawat102@gmail.com>
2020-07-23 19:57:30 +0530

    update scrapping

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,44 +1,43 @@
-const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const colors = require("colors");
-module.exports={
-    
-  flipkartFetch: async () => {
-    const body = {
-      a: 1,
-    };
-      fetch(
-        "https://www.flipkart.com/search?q=mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_7_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_7_na_na_na&as-pos=1&as-type=HISTORY&suggestionId=mobiles%7CMobiles&requestId=c101808f-c71a-4609-b769-69471c4e9b5a",
-        {
-          method: "post",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+const fs = require("fs");
+const json2csv = require("json2csv").Parser;
+module.exports = {
+  flipkartFetch: () => {
+    axios
+      .get(
+        "https://www.flipkart.com/search?q=mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_7_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_7_na_na_na&as-pos=1&as-type=HISTORY&suggestionId=mobiles%7CMobiles&requestId=c101808f-c71a-4609-b769-69471c4e9b5a"
       )
-        .then((res) => res.text())
-        .then((Result) => console.log(Result));
-  },
-  snapdealFetch: () => {
-    const body = {
-      a: 1,
-    };
-    fetch(
-      "https://www.snapdeal.com/search?keyword=tshirts&santizedKeyword=&catId=&categoryId=0&suggested=false&vertical=&noOfResults=20&searchState=&clickSrc=go_header&lastKeyword=&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&categoryUrl=&url=&utmContent=&dealDetail=&sort=rlvncy/post",
-      {
-        method: "post",
-        body: JSON.stringify(body),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => res.text())
-      .then((json) => console.log(json));
-  },
+      .then((fetched) => {
+        const $ = cheerio.load(fetched.data);
+        var arr = [];
+        let name = $('div[class="col col-7-12"]> div[class="_3wU53n"]').text();
+        let rating = $('div[class="hGSR34"]').text();
+        let img = $(
+          "#container > div > div.t-0M7P._2doH3V > div._3e7xtJ > div._1HmYoV.hCUpcT > div:nth-child(2) > div:nth-child(2) > div > div > div > a > div._3SQWE6 > div._1OCn9C > div > div > img"
+        )
+          .attr("src")
+          .trim();
 
+        let price = $('div[class="_1uv9Cb"]>[class="_1vC4OE _2rQ-NK"]')
+          .text()
+          .split("₹");
+        let discount = $('div[class="VGWI6T"]> span').text().split(" ");
+        arr.push({
+          name,
+          rating,
+          img,
+          price,
+          discount,
+        });
+        const jcp = new json2csv();
+        const csv = jcp.parse(arr);
+
+        fs.writeFileSync("./datafile.csv", csv, "utf-8");
+        console.log(arr);
+      });
+  },
   Urls: (req, res) => {
     axios
       .get(
@@ -46,18 +45,14 @@ module.exports={
       )
       .then((result) => {
         const $ = cheerio.load(result.data);
-        var DATA =[]
+        const data = [];
         const name = $(
           'div[class="col col-7-12"]> div[class="_3wU53n"]'
         ).text();
-        DATA.push(name)
-        // res.send(name.blue);
-        DATA.forEach((element)=>{ 
-            res.send(element.split(')'))
-            
-        })
-        
+        data.push(name);
+        data.forEach((element) => {
+          res.send(element.split(")"));
+        });
       });
-
   },
-}
+};
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ var usercontroller = require("../controller/usercontroller");
 var router = express.Router();
 
 router.get("/fetch", usercontroller.flipkartFetch);
-router.get("/snapfetch", usercontroller.snapdealFetch);
 router.get("/url", usercontroller.Urls);
 
 module.exports = router;
