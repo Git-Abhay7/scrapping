56abb9c42802c8fd1f8db1e3e04d710248f9ecd6
Git-Abhay7 <abhayrajawat102@gmail.com>
2020-07-31 17:40:40 +0530

    AIRPORT APIS

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,12 @@ var usercontroller = require("../controller/usercontroller");
 var router = express.Router();
 
 router.get("/fetch", usercontroller.flipkartFetch);
-router.get("/url", usercontroller.Urls);
+router.get("/url", usercontroller.airportFetch);
+router.post("/distance", usercontroller.Distance);
+router.get("/nearest", usercontroller.nearestAirport);
+
+
+
 
 module.exports = router;
 
